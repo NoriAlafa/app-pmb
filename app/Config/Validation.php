bb6946770559c555799d2a2a074ff8553332882c
NoriAlafa <alafanori@gmail.com>
2021-07-14 19:16:33 +0700

    [EDIT]Menambah Validasi Info Pendaftaran & Pendaftaran Buat Akun

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -62,4 +62,54 @@ class Validation
 			]
 		]
 	];
+
+	//validasi informasi pendaftara
+	public $informasi = [
+		'tgl_pendaftaran' =>[
+			'label'		  =>'Tanggal Pendaftaran',
+			'rules'		  =>'required',
+			'errors'	  =>[
+				'required'=>'Tanggal pendaftaran tidak boleh kosong'
+			]	  
+		],
+		'tgl_pengumuman'  =>[
+			'label'		  =>'Tanggal Pengumuman',
+			'rules'		  =>'required',
+			'erros'		  =>[
+				'required'=>'Tanggal pengumuman pendaftaran tidak boleh kosong'
+			]
+		]
+	];
+
+	//validasi pendaftaran buat akun
+	public $daftar_akun = [
+		'nama'=>[
+			'label'		  =>'Nama Lengkap',
+			'rules'		  =>'required',
+			'errors'	  =>[
+				'required'=>'Nama Lengkap tidak boleh kosong'
+			]
+		],
+		'email'=>[
+			'label'		  =>'E-mail',
+			'rules'		  =>'required|valid_email|is_unique[tbl_user.email]',
+			'errors'	  =>[
+				'required'=>'Email tidak boleh kosong!',
+				'valid_email'=>'Email tidak valid!',
+				'is_unique'=>'Email sudah terdaftar'
+			]
+		],
+		'password'=>[
+			'label'		  =>'Password',
+			'rules'		  =>'required|min_length[8]',
+			'errors'	  =>[
+				'required'=>'Password tidak boleh kosong!',
+				'min_length'=>'Password minimal 8 karakter'
+			]
+		],
+		'confirm_password'=>[
+			'label'	      =>'Konfirmasi Password',
+			'rules'		  =>'required|min_length[8]|matches[password]'
+		]
+	];
 }
