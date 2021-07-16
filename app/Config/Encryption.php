08d6254c1625c5b46784950aa2425104063e3865
NoriAlafa <alafanori@gmail.com>
2021-07-16 18:48:00 +0700

    [EDIT]Menambah Enskripsi password user

--- app/Config/Encryption.php
+++ app/Config/Encryption.php
@@ -23,7 +23,7 @@ class Encryption extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $key = '';
+	public $key = '20app-pmb20';
 
 	/**
 	 * --------------------------------------------------------------------------
