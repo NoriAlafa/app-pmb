8ee78d77b085780fe0dbea5a42968f67dcffde80
NoriAlafa <alafanori@gmail.com>
2021-07-19 18:37:44 +0700

    [EDIT]Menambah Header

---
+++ app/Views/v_login/index.php
@@ -0,0 +1,13 @@
+<?=$this->extend('layouts_peserta/template_peserta')?>
+
+<?=$this->section('header')?>
+    <header id="header" class="fixed-top">
+        <div class="container d-flex align-items-center">
+            <h1 class="logo mr-auto">
+                <a href="<?=base_url('/');?>">
+                    <span>Alfa University</span>
+                </a>
+            </h1>
+        </div>
+    </header>
+<?=$this->endSection()?>
