841da86f3b6515f14eb6f2d975970041ba9cc0d5
NoriAlafa <alafanori@gmail.com>
2021-07-20 20:13:03 +0700

    [EDIT]Menambah funsi index dan logout

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1,3 +1,28 @@
 <?php
+namespace App\Controller;
+class Dashboard extends BaseController{
+    protected $session;
 
+    
+    public function __construct()
+    {
+        $this->session = \Config\Services::session();
+    }
+
+    //halaman dashboard
+    public function index(){
+        $data['title']="Hi Mister | Dashboard";
+        $data['page']="dashboard";
+        $data['nama']=$this->session->get('nama');
+        $data['email']=$this->session->get('email');
+        return view('v_dashboard/index',$data);
+    }
+
+    //logout
+    public function logout(){
+        $this->session->destroy();
+        return redirect()->to('/');
+    }
+    
+}
 ?>
