6a9a9692ac76090ab276a09e1000032be78ae9ea
NoriAlafa <alafanori@gmail.com>
2021-07-21 13:33:33 +0700

    [EDIT]Menambah content wrapper

--- app/Views/v_dashboard/index.php
+++ app/Views/v_dashboard/index.php
@@ -1,5 +1,36 @@
 <?=$this->extends('layouts_admin/template_admin');?>
 
 <?=$this->section('content')?>
-
+    <!-- content wrapper . contains page content -->
+    <div class="content-wrapper">
+        <!-- content header/page header -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-12">
+                        <h1>Dashboard</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--Main content  -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg">
+                        <!-- Small box -->
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>PMB|ALFA</h3>
+                                <p>Pendaftaran mahasiswa baru - Alfa University</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-bag"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 <?=$this->endSection()?>
