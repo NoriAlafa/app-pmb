50ea127fd8b627b0318ad14824f26ef03ca5a42b
NoriAlafa <alafanori@gmail.com>
2021-07-26 14:02:06 +0700

    [EDIT]Menambah tombol aksi pada tabel data fakultas

--- app/Controllers/DataFakultas.php
+++ app/Controllers/DataFakultas.php
@@ -2,4 +2,40 @@
 namespace App\Controllers;
 use App\Models\FakultasModel;
 use Config\Services;
+
+class DataFakultas extends BaseController{
+    protected $M_fakultas;
+    protected $request;
+    protected $form_validation;
+    protected $session;
+    
+    
+    public function __construct()
+    {
+        $this->request         = Services::request();
+        $this->M_fakultas      = new FakultasModel($this->request);
+        $this->form_validation = \Config\Services::validation();
+        $this->session         = \Config\Services::session();
+    }
+
+    // Tombol aksi pada tabel data fakultas
+    private function _action($idFakultas){
+        $link = "<a data-toggle='tooltip' data-placement='top' class='btn-editFakultas' title='edit'
+                    value='.$idFakultas'>
+                    <button type='button' class='btn btn-outline-success btn-xs' data-toggle='modal'
+                    data-target='#modalEdit'>
+                        <i class='fa fa-edit'></i>
+                    </button>
+                </a>
+                <a href='".base_url('datafakultas/delete/'.$idFakultas);"' data-toggle='tooltip' data-placement='top' class='btn-deleteFakultas' title='delete'>
+                    <button type='button' class='btn btn-outline-danger btn-xs' data-toggle='modal'
+                    data-target='#modalEdit'>
+                        <i class='fa fa-trash'></i>
+                    </button>
+                </a>";              
+        return $link;
+    }
+
+    
+}
 ?>
