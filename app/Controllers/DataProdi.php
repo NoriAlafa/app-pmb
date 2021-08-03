57f6ac7b3556dfda4f1d92ee9c4ef892fb815921
NoriAlafa <alafanori@gmail.com>
2021-08-03 18:25:34 +0700

    [EDIT]Construct

--- app/Controllers/DataProdi.php
+++ app/Controllers/DataProdi.php
@@ -4,4 +4,22 @@ use App\Models\FakultasModel;
 use App\Models\ProdiModel;
 use Config\Services;
 
+class DataProdi extends BaseController{
+    protected $M_fakultas;
+    protected $M_prodi;
+    protected $request;
+    protected $form_validation;
+    protected $session;
+
+    
+    public function __construct()
+    {
+        $this->request          = Services::request();
+        $this->M_fakultas       = new FakultasModel($this->request);
+        $this->M_prodi          = new ProdiModel($this->request);
+        $this->form_validation  = \Config\Services::validation();
+        $this->session          = \Config\Services::session();
+    }
+    
+}
 ?>
