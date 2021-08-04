16e01d788b660dae72be8b7a0cffebf0592ece1c
NoriAlafa <alafanori@gmail.com>
2021-08-04 12:45:29 +0700

    [EDIT]Tombol aksi pada tabel data prodi

--- app/Controllers/DataProdi.php
+++ app/Controllers/DataProdi.php
@@ -21,5 +21,20 @@ class DataProdi extends BaseController{
         $this->session          = \Config\Services::session();
     }
     
+    // tombol aksi pada tabel data prodi
+    private function _action($idProdi){
+        $link ="
+            <a data-toggle='tooltip' data-placement='top' class='btn-editProdi' title='Update' value='".$idProdi"'>
+                <button type='button' class='btn btn-outline-success btn-xs' data-toggle='modal' data-target='#modalEdit'>
+                    <i class='fa fa-edit'></i>
+                </button>
+            </a>
+            <a href='".base_url('dataprodi/delete'.$idProdi)".' data-toggle='tooltip' data-placement='top' class='btn-deleteProdi' title='Delete'>
+                <button type='button' class='btn btn-outline-danger btn-xs'>
+                    <i class='fa fa-trash'></i>
+                </button>
+            </a>";
+        return $link;
+    }
 }
 ?>
