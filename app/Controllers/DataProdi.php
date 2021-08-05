0740558f16497c3dc11c027b47b4780f86f43fd6
NoriAlafa <alafanori@gmail.com>
2021-08-05 13:01:21 +0700

    [EDIT]Add data prodi

--- app/Controllers/DataProdi.php
+++ app/Controllers/DataProdi.php
@@ -24,12 +24,12 @@ class DataProdi extends BaseController{
     // tombol aksi pada tabel data prodi
     private function _action($idProdi){
         $link ="
-            <a data-toggle='tooltip' data-placement='top' class='btn-editProdi' title='Update' value='".$idProdi"'>
+            <a data-toggle='tooltip' data-placement='top' class='btn-editProdi' title='Update' value='".$idProdi."'>
                 <button type='button' class='btn btn-outline-success btn-xs' data-toggle='modal' data-target='#modalEdit'>
                     <i class='fa fa-edit'></i>
                 </button>
             </a>
-            <a href='".base_url('dataprodi/delete'.$idProdi)".' data-toggle='tooltip' data-placement='top' class='btn-deleteProdi' title='Delete'>
+            <a href='".base_url('dataprodi/delete'.$idProdi)."' data-toggle='tooltip' data-placement='top' class='btn-deleteProdi' title='Delete'>
                 <button type='button' class='btn btn-outline-danger btn-xs'>
                     <i class='fa fa-trash'></i>
                 </button>
@@ -54,6 +54,36 @@ class DataProdi extends BaseController{
             return view('v_dataProdi/index',$data);
         }else{
             return view('v_dataProdi/error',$data);
+        }
+    }
+
+    // Add data prodi
+    public function add(){
+        $fakultas_id    = $this->request->getPost('fakultas_id');
+        $nama_prodi     = ucwords($this->request->getPost('nama_prodi'));
+
+        // data prodi
+        $data = [
+            'fakultas_id'   =>$fakultas_id,
+            'nama_prodi'    =>$nama_prodi
+        ];
+
+        // cek validasi data prodi ,jika data tidak valid
+        if($this->form_validation->run($data,'prodi')==FALSE){
+            $validasi = [
+                'error'             =>true,
+                'nama_prodi_error'  =>$this->form_validation->getErrors('nama_prodi')
+            ];
+            echo json_encode($validasi);
+        }
+        // data valid
+        else{
+            // simpan data prodi
+            $this->M_prodi->save($data);
+            $validasi = [
+                'success'   =>true 
+            ];
+            echo json_encode($validasi);
         }
     }
 }
