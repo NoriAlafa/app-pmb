03278ec9bb9a743fbf75b3ce987e6975a6e5ee36
NoriAlafa <alafanori@gmail.com>
2021-08-17 19:13:35 +0700

    [EDIT]Construct

--- app/Controllers/DataPendaftaran.php
+++ app/Controllers/DataPendaftaran.php
@@ -4,4 +4,23 @@ use App\Models\PendaftaranModel;
 use App\Models\FakultasModel;
 use App\Models\ProdiModel;
 use Config\Services;
+
+class DataPendaftaran extends BaseController{
+    protected $session;
+    protected $M_pendaftaran;
+    protected $M_fakultas;
+    protected $M_prodi;
+    protected $request;
+
+    
+    public function __construct()
+    {
+        $this->request = Services::request();
+        $this->M_pendaftaran = new PendaftaranModel($this->request);
+        $this->M_fakultas = new FakultasModel($this->request);
+        $this->M_prodi = new ProdiModel($this->request);
+        $this->session = \Config\Services::session();
+    }
+    
+}
 ?>
