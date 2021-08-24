be6a1fc2ae9d2eacd0005c25a509fe10b6107b38
NoriAlafa <alafanori@gmail.com>
2021-08-24 19:20:26 +0700

    [EDIT]Tombol aksi pada tabel pendaftaran 1

--- app/Controllers/DataPendaftaran.php
+++ app/Controllers/DataPendaftaran.php
@@ -21,6 +21,19 @@ class DataPendaftaran extends BaseController{
         $this->M_prodi = new ProdiModel($this->request);
         $this->session = \Config\Services::session();
     }
+
+    // Tombol aksi pada tabel pendaftaran
+    private function _action($idPendaftaran,$statusVerifikasi){
+        if($statusVerifikasi == "Lulus" || $statusVerifikasi == "Tidak Lulus"){
+            $link = 
+            "<a href='".base_url('datapendaftaran/view/'.$idPendaftaran)."' class='btn-viewPendaftaran' data-toggle='tooltip' data-placement='top' title='View'>
+                <button type='button' class='btn btn-outline-primary btn-xs'>
+                    <i class='fas fa-eye'></i>
+                </button>
+            </a>";
+            return $link;
+        }
+    }
     
 }
 ?>
