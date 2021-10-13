36d38f7fe996803dabe8b2f7e68b202b7d1500b4
NoriAlafa <alafanori@gmail.com>
2021-10-13 08:50:32 +0700

    [EDIT]PendaftaranModel

--- app/Models/PendaftaranModel.php
+++ app/Models/PendaftaranModel.php
@@ -12,6 +12,74 @@ class PendaftaranModel extends Model{
         'nomor_pendaftaran',
         'nama_peserta',
         'tempat_lahir',
+        'tanggal_lahir',
+        'jenis_kelamin',
+        'agama',
+        'no_hp',
+        'alamat_peserta',
+        'nama_orangtua',
+        'pekerjaan_orangtua',
+        'no_hp_orangtua',
+        'nama_sekolah',
+        'tahun_lulus',
+        'alamat_sekolah',
+        'foto',
+        'berkas',
+        'tahap_satu',
+        'tahap_dua',
+        'tahap_tiga',
+        'tanggal_pendaftaran',
+        'status_pendaftaran',
+        'status_verifikasi'
     ];
+    protected $useTimeStamps    = true;
+    protected $column_order     = [null,'nomor_pendaftaran','tanggal_pendaftaran','status_verifikasi',null];
+    protected $column_search    = ['nomor_pendaftaran','nama_prodi','nama_peserta','tanggal_pendaftaran','status_verifikasi'];
+    protected $order            = ['tbl_pendaftaran.id'=>'desc'];
+    protected $request;
+    protected $db;
+    protected $dt;
+
+    
+    function __construct(RequestInterface $request)
+    {
+        parent::__construct();
+        $this->db       = db_connect();
+        $this->request  = $request;
+        $this->dt       = $this->db->table($this->table)
+                            ->select('tbl_pendaftaran.id, nomor_pendaftaran, tanggal_pendaftaran, status_verifikasi')
+                            ->join('tbl_prodi','tbl_prodi.id = tbl_pendaftaran.prodi_id','left')
+                            ->where('status_pendaftaran' , "Selesai");
+    }
+
+    private function _get_datatables_query(){
+        $i = 0;
+        foreach ($this->column_search as $item){
+            if($this->request->getPost('search')['value']){
+                if($i===0){
+                    $this->dt->groupStart();
+                    $this->dt->like($item,$this->request->getPost('search')['value']);
+                }
+                else{
+                    $this->dt->orLike($item,$this->request->getPost('search')['value']);
+                }
+
+                if(count($this->column_search) - 1 ==$i){
+                    $this->dt->groupEnd();
+                }
+
+            }
+        }
+
+        if($this->request->getPost('order')){
+            $this->dt->orderBy($this->column_order[$this->request->getPost('order')['0']['column']],
+            $this->request->getPost('order')['0']['dir']);
+        }
+        else if(isset($this->order)){
+            $order = $this->order;
+            $this->dt->orderBy(key($order),$order[key($order)]);
+        }
+    }
+    
 }
 ?>
